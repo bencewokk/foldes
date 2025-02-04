0a9d8aa008f093f073be9d22d49596c24f235d55
bencewok <bencewok@gmail.com>
2025-02-04 16:34:15 +0100

    0.6

--- foldestheme/page-tanaraink.php
+++ foldestheme/page-tanaraink.php
@@ -16,63 +16,48 @@ get_header();
         ?>
         
         <?php
-        // Define teacher data
-        $teachers = [
-            'fazekas' => [
-                'name' => 'Fazekas Péter',
-                'title' => 'Matematika Tanár',
-                'bio' => '10+ év tapasztalat, OKJ tanár, kreatív módszerek',
-            ],
-            'csato' => [
-                'name' => 'Csató László',
-                'title' => 'Fizika Tanár',
-                'bio' => 'PhD, egyetemi oktató, versenyfelkészítő',
-            ],
-            'pilz' => [
-                'name' => 'Pilz Katalin',
-                'title' => 'Biológia Tanár',
-                'bio' => 'Természettudományok szakértő, projektalapú oktatás',
-            ],
-            'chalu' => [
-                'name' => 'Chalu Péter',
-                'title' => 'Informatika Tanár',
-                'bio' => 'Full-stack fejlesztő, programozási versenyek szervezője',
-            ],
-            'lorincz' => [
-                'name' => 'Lőrincz Anna',
-                'title' => 'Magyar Nyelv és Irodalom Tanár',
-                'bio' => 'Író, költő, kreatív írás szakértő',
-            ],
-            'kondi' => [
-                'name' => 'Kondi Éva',
-                'title' => 'Angol Nyelv Tanár',
-                'bio' => 'Cambridge vizsgakészítő, anyanyelvi szintű angol',
-            ],
-        ];
+        $teacher_query = new WP_Query(array(
+            'post_type'      => 'post',
+            'tag'           => 'tanar',
+            'posts_per_page' => -1,
+            'orderby'       => 'menu_order',
+            'order'         => 'ASC'
+        ));
 
-        if (!empty($teachers)) :
+        if ($teacher_query->have_posts()) :
         ?>
             <div class="teachers-grid">
-                <?php foreach ($teachers as $slug => $teacher) : ?>
+                <?php while ($teacher_query->have_posts()) : $teacher_query->the_post(); 
+                    $teacher_title = get_post_meta(get_the_ID(), 'teacher_title', true);
+                ?>
                     <article class="teacher-card">
                         <div class="image-container">
-                            <img src="<?php echo esc_url(get_template_directory_uri() . "/tanaraink/tanarok/{$slug}.jpg"); ?>" 
-                                 alt="<?php echo esc_attr($teacher['name']); ?>">
+                            <?php if (has_post_thumbnail()) : ?>
+                                <img src="<?php the_post_thumbnail_url('medium_large'); ?>" 
+                                     alt="<?php the_title_attribute(); ?>">
+                            <?php endif; ?>
                             <div class="teacher-overlay">
                                 <div class="teacher-overlay-content">
-                                    <h4><?php echo esc_html($teacher['title']); ?></h4>
-                                    <ul>
-                                        <li><?php echo esc_html($teacher['bio']); ?></li>
-                                    </ul>
+                                    <h4><?php echo esc_html($teacher_title); ?></h4>
+                                    <div class="teacher-bio">
+                                        <?php echo wp_kses_post(get_the_excerpt()); ?>
+                                    </div>
                                 </div>
                             </div>
                         </div>
                         <div class="teacher-info">
-                            <h3 class="teacher-name"><?php echo esc_html($teacher['name']); ?></h3>
+                            <h3 class="teacher-name"><?php the_title(); ?></h3>
+                            <!-- Button added here under the name -->
+                            <a href="<?php echo esc_url(get_permalink()); ?>" class="teacher-button">
+                                Bővebben
+                            </a>
                         </div>
                     </article>
-                <?php endforeach; ?>
+                <?php endwhile; ?>
             </div>
+            <?php wp_reset_postdata(); ?>
+        <?php else : ?>
+            <p class="no-teachers"><?php esc_html_e('Nincsenek tanárok megjeleníthetőek.', 'textdomain'); ?></p>
         <?php endif; ?>
     </div>
 </main>
