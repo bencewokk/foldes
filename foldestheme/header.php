5926a007babd40bb0f556c504fb7b1ad4b7a939d
Bencewok <bencewokk@gmail.com>
2025-02-07 23:54:19 +0100

    1.0.1

--- foldestheme/header.php
+++ foldestheme/header.php
@@ -15,13 +15,12 @@
         <nav id="main-navigation">
             <ul class="custom-menu">
                 <li><a href="<?php echo esc_url(home_url('/')); ?>">Otthon</a></li>
-                <li><a href="<?php echo esc_url(home_url('/szuloknek')); ?>">Szülőknek</a></li>
+                <li><a href="<?php echo esc_url(home_url('/iskolankrol')); ?>">Iskolánkról</a></li>
                 <li><a href="<?php echo esc_url(home_url('/tanaraink')); ?>">Tanáraink</a></li>
-                <li><a href="<?php echo esc_url(home_url('/osztalyaink')); ?>">Osztályaink</a></li>
                 <li><a href="<?php echo esc_url(home_url('/tablok')); ?>">Tablók</a></li>
                 <li><a href="<?php echo esc_url(home_url('/versenyek')); ?>">Versenyek</a></li>
-                <li><a href="<?php echo esc_url(home_url('/iskolankrol')); ?>">Iskolánkról</a></li>
-                <li><a href="<?php echo esc_url(home_url('/diakoknak')); ?>">Diákoknak</a></li>
+                <li><a href="<?php echo esc_url(home_url('/szuloknek')); ?>">Szülőknek</a></li>
+
 
                 <div class="gtranslate_wrapper">
                     <?php echo do_shortcode('[gtranslate]'); ?> 
